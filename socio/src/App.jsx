5a446128429fc468d836002ca114d1630a2fa083
Developer-Madhu <madhuatomix@gmail.com>
2025-04-03 22:03:00 +0530

    created navbar component

--- socio/src/App.jsx
+++ socio/src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core';
+import HomePage from './HomePage';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div>
-      
-    </div>
+    <MantineProvider>
+      <HomePage />
+    </MantineProvider>
   )
 }
 
